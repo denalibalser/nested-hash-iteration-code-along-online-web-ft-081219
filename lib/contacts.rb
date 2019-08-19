c76f82c888aa52ea2f2971fb2b8c5c3659dc37d2
Denali Balser <denali.balser167@gmail.com>
2019-08-19 13:24:17 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,14 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
+def remove_strawberry(contacts)
+  contacts.each do |name, info|
+    if name == "Freddy Mercury"
+      info.each do |category, items|
+      if category == "favorite_ice_cream_flavors"
+        items.delete[-1]
+      end
+    end
+  end 
 end
